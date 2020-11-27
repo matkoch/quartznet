e94f2ca5c745db7a5f5fc6d929a7c5cda3ac9fa3
Matthias Koch <ithrowexceptions@gmail.com>
2020-11-27 20:29:24 +0100

    Add NUKE

---
+++ build/Build.cs
@@ -0,0 +1,105 @@
+﻿using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AppVeyor;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[AppVeyor(
+    AppVeyorImage.VisualStudioLatest,
+    AutoGenerate = false,
+    Services = new[] {AppVeyorService.MYSQL, AppVeyorService.PostgreSQL})]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    // [GitVersion(UpdateBuildNumber = true)] readonly GitVersion GitVersion;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath BinaryDirectory => ArtifactsDirectory / "bin";
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(BinaryDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild());
+        });
+
+    AbsolutePath PackagesDirectory => ArtifactsDirectory / "packages";
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Produces(PackagesDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .SetProject(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .SetOutputDirectory(PackagesDirectory));
+        });
+
+    readonly string NuGetSource = "https://api.nuget.org/v3/index.json";
+    [Parameter] readonly string NuGetApiKey;
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Requires(() => NuGetApiKey)
+        .Executes(() =>
+        {
+            DotNetNuGetPush(_ => _
+                    .SetSource(NuGetSource)
+                    .SetApiKey(NuGetApiKey)
+                    .CombineWith(PackagesDirectory.GlobFiles("*.nupkg"), (_, v) => _
+                        .SetTargetPath(v)),
+                completeOnFailure: true,
+                degreeOfParallelism: 5);
+        });
+}
